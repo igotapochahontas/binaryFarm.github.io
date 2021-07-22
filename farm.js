9d150b02ea812dd2eeb1fc5358f2c7688d8734fb
igotapochahontas <igotapochahontas@yahoo.com>
2021-07-21 20:58:52 -0500

    Update farm.js

--- farm.js
+++ farm.js
@@ -2,10 +2,18 @@ let world = world();
 
 function world(){
 alert("hello world");
-tools(){
+// maps
+tools{
+   transistor: true; 
+   seeds{
+and: true, 
+or: true, 
+xor: true; 
+}
 }
 hero{
-
+    health:high, 
+    wealth:3;
 }
 npc{
 }
